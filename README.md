8dc8ce624d03008f22870c4fd80149ffd4bfbbb7
depaulmillz <dsm220@lehigh.edu>
2023-04-12 13:21:37 -0400

    added optitrack

---
+++ README.md
@@ -0,0 +1,3 @@
+# Robotics CSE 460 Final
+
+
---
+++ optitrack/src/optitrack_test.py
@@ -0,0 +1,46 @@
+import sys
+import time
+import socket
+from NatNetClient import NatNetClient
+from util import quaternion_to_euler_angle_vectorized1
+
+positions = {}
+rotations = {}
+
+
+# This is a callback function that gets connected to the NatNet client. It is called once per rigid body per frame
+def receive_rigid_body_frame(robot_id, position, rotation_quaternion):
+    # Position and rotation received
+    positions[robot_id] = position
+    # The rotation is in quaternion. We need to convert it to euler angles
+
+    rotx, roty, rotz = quaternion_to_euler_angle_vectorized1(rotation_quaternion)
+
+    rotations[robot_id] = rotz
+
+
+if __name__ == "__main__":
+    # hostname = socket.gethostname()
+    # ip_addr = socket.gethostbyname(hostname)
+    clientAddress = "0.0.0.0"
+    optitrackServerAddress = "192.168.0.4"
+    robot_id = 207
+
+    # This will create a new NatNet client
+    streaming_client = NatNetClient()
+    streaming_client.set_client_address(clientAddress)
+    streaming_client.set_server_address(optitrackServerAddress)
+    streaming_client.set_use_multicast(True)
+    # Configure the streaming client to call our rigid body handler on the emulator to send data out.
+    streaming_client.rigid_body_listener = receive_rigid_body_frame
+
+    # Start up the streaming client now that the callbacks are set up.
+    # This will run perpetually, and operate on a separate thread.
+    is_running = streaming_client.run()
+    while is_running:
+        print(positions.keys())
+        if robot_id in positions:
+            # last position
+            print('Last position', positions[robot_id], ' rotation', rotations[robot_id])
+
+            time.sleep(1)
