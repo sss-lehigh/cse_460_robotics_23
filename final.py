b874d5bb429ca6bb7a4357282ceabd2774c9772b
dePaul Miller <depaulmillz@users.noreply.github.com>
2023-05-04 12:40:06 -0400

    Updated to add states

--- final.py
+++ final.py
@@ -1,43 +1,97 @@
 from apis import *
+import time
+from enum import Enum
 
-# bounds
-
-bounds = [[-4.39, 3.73],
-          [-4.39, -2.14],
-          [6.0, -2.14],
-          [6.0, 3.73]]
+class State(Enum):
+    EXPLORING = 1
+    CAPTURING = 2
+    RETURNING_TO_SAFETY = 3
+    RETURNING_TO_EXPLORE = 4
 
 if __name__ == "__main__":
    
-    robot_id = 207
-
-    robot = LocalRobot()
-    position = Position("192.168.0." + str(robot_id), "192.168.0.4", robot_id)
+    robot = LocalRobot() 
+    position = Position("192.168.0.207", "192.168.0.172", 307)
+    grid = Discretization(-3.3, -.26, -.46, 2.63)
 
     x_d = np.array([0., 0.])
-    
-    try:
-        while True:
-            x_t, angle = position.get()
 
-            print(x_t)
+    x_t, angle = position.get()
 
-            K1 = 1000
-            K2 = 1500
+    curr_node = grid.get_nearest_node(x_t)
 
-            dist, desired = dist_and_angle(x_d, x_t)
+    path = grid.get_path(curr_node, 30)
 
-            delta_angle = angle_diff(desired, angle)
+    K1 = 1000
+    K2 = 1500
 
-            v = K1 * dist
-            w = K2 * delta_angle
-            u = np.array([0.,0.])
-            #u = np.array([v - w, v + w])
+    node_idx = 1
 
-            robot.set_motor(u[0], u[0], u[1], u[1])
+    start = time.time()
 
-    except (Exception, KeyboardInterrupt) as e:
-        print(type(e))
+    init = np.copy(x_t)
+
+    init = np.zeros(2)
+    slope = np.zeros(2)
+
+    if node_idx < len(path):
+        init = x_t
+        slope = path[node_idx] - init
+    else:
+        slope = np.zeros(2)
+        init = path[-1]
+
+    factor = 2.0
 
     robot.stop_motor()
-    robot.shutdown()
+
+    state = State.EXPLORING
+
+    try:
+        while True:
+
+            if state == State.EXPLORING: 
+
+                x_t, angle = position.get()
+
+                curr_time = time.time()
+
+                if (curr_time - start) / factor > 1.0:
+                    start = curr_time
+                    node_idx += 1
+                    if node_idx < len(path):
+                        init = x_t
+                        slope = path[node_idx] - init
+                    else:
+                        slope = np.zeros(2)
+                        init = path[-1]
+
+                x_d = ((curr_time - start) / factor) * slope + init
+
+                dist, desired = dist_and_angle(x_d, x_t)
+
+                delta_angle = angle_diff(desired, angle)
+
+                v = K1 * dist
+                w = K2 * delta_angle
+                u = np.array([v - w, v + w])
+
+                robot.set_motor(u[0], u[0], u[1], u[1])
+            elif state == State.CAPTURING:
+                pass
+            elif state == State.RETURNING_TO_SAFETY:
+                pass
+            elif state == State.RETURNING_TO_EXPLORE:
+                pass
+            else:
+                print("Error")
+
+            time.sleep(0.1)
+
+    except KeyboardInterrupt as e:
+        print("Sending stop")
+        robot.set_motor(0.0, 0.0, 0.0, 0.0)
+        robot.stop_motor()
+        robot.shutdown()
+
+    os._exit(0)
